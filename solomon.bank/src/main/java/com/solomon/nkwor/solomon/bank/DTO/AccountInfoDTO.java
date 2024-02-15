c86eb5eb8cf4d4a8cce9bba1a1e1971d15f18c62
solomon-nkwor <solomonnkwor6@gmail.com>
2024-02-15 16:59:47 +0100

    included schemas for DTOs in the documentation

--- solomon.bank/src/main/java/com/solomon/nkwor/solomon/bank/DTO/AccountInfoDTO.java
+++ solomon.bank/src/main/java/com/solomon/nkwor/solomon/bank/DTO/AccountInfoDTO.java
@@ -1,5 +1,6 @@
 package com.solomon.nkwor.solomon.bank.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,10 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccountInfoDTO {
+    @Schema(name = "User account name")
     private String accountName;
+    @Schema(name = "User account balance")
     private BigDecimal accountBalance;
+    @Schema(name = "User account number")
     private String accountNumber;
 }
